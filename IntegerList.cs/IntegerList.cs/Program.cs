f061b57ca6512b9567f2e66eb1636d772aa267af
Jazz-Aka-Amazing <Jazsingh12825@gmail.com>
2014-03-27 09:44:28 -0400

    Day1

---
+++ IntegerList.cs/IntegerList.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[8];
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Array.Sort(grades);
+                Array.Reverse(grades);
+                Console.WriteLine(grades.Length);
+
+            }
+        }
+    }
+}
